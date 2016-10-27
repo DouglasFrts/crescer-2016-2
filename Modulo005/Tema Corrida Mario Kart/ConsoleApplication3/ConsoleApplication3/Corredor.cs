492c9340eebbc30a21f3b82e6be9ff016fd3d92a
Unknown <douglasdefreitass@gmail.com>
2016-10-27 17:40:53 -0200

    feat(C#Exemplo, Exercicio): Criar projeto para o exercicio de Corrida Mario Kart.

---
+++ Modulo005/Tema Corrida Mario Kart/ConsoleApplication3/ConsoleApplication3/Corredor.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApplication3.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corrida
+{
+    public class Corredor
+    {
+        enum Niveis
+        {
+            
+        };
+
+        public Corredor(String nome, NivelHabilidade nivel){
+           this.Nome = nome;
+            this.Nivel = nivel;
+            
+        }
+
+        private String Nome{ get; }
+
+        public  NivelHabilidade Nivel;
+
+    }
+}
